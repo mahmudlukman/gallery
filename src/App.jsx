dff675b548d5cb88eb43f6b442db829f9a0bf8ab
Mahmud Lukman <mistamann49@gmail.com>
2023-07-30 15:43:53 +0100

    Nav component completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import { Container } from "@mui/material"
+import { Nav } from "./components/Nav"
+import ImagesList from "./components/imagesList/ImagesList"
+import Upload from "./components/upload/Upload"
 
 function App() {
 
   return (
-    <div>
-      App
-    </div>
+    <Container maxWidth='lg' sx={{textAlign: 'center', mt: '3rem'}}>
+      <Nav/>
+      <Upload/>
+      <ImagesList/>
+    </Container>
   )
 }
 
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,95 @@
-import React from 'react'
+import {useState} from 'react';
+import {Box, Avatar, Menu, MenuItem, ListItemIcon, Divider, IconButton, Typography, Tooltip, Button } from '@mui/material';
+import { Settings, Logout, Lock} from '@mui/icons-material';
+import profileImg from '../img/profile.jpg'
 
 export const Nav = () => {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (e) => {
+    setAnchorEl(e.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  
+  const [currentUser, setCurrentUser] = useState({
+    email: 'test@test.com',
+    displayName: 'John',
+    photoURL: profileImg,
+  })
   return (
-    <div>Nav</div>
-  )
+    <>
+      <Box sx={{ display: 'flex', alignItems: 'center', textAlign: 'center' }}>
+        {!currentUser ? <Button startIcon={<Lock/>}>Login</Button> : (
+            <Tooltip title="Account settings">
+            <IconButton
+              onClick={handleClick}
+              size="small"
+              sx={{ ml: 2 }}
+              aria-controls={open ? 'account-menu' : undefined}
+              aria-haspopup="true"
+              aria-expanded={open ? 'true' : undefined}
+            >
+              <Avatar sx={{ width: 32, height: 32 }} src={currentUser?.photoURL}>
+                {currentUser?.displayName?.charAt(0)?.toUpperCase() || currentUser?.email?.charAt(0)?.toUpperCase()}
+              </Avatar>
+            </IconButton>
+          </Tooltip>
+        )}
+      </Box>
+      <Menu
+        anchorEl={anchorEl}
+        id="account-menu"
+        open={open}
+        onClose={handleClose}
+        onClick={handleClose}
+        slotProps={{
+          elevation: 0,
+          sx: {
+            overflow: 'visible',
+            filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+            mt: 1.5,
+            '& .MuiAvatar-root': {
+              width: 32,
+              height: 32,
+              ml: -0.5,
+              mr: 1,
+            },
+            '&:before': {
+              content: '""',
+              display: 'block',
+              position: 'absolute',
+              top: 0,
+              right: 14,
+              width: 10,
+              height: 10,
+              bgcolor: 'background.paper',
+              transform: 'translateY(-50%) rotate(45deg)',
+              zIndex: 0,
+            },
+          },
+        }}
+        transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+        anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+      >
+        <MenuItem onClick={handleClose}>
+          <Avatar /> Profile
+        </MenuItem>
+        <Divider />
+        <MenuItem onClick={handleClose}>
+          <ListItemIcon>
+            <Settings fontSize="small" />
+          </ListItemIcon>
+          Settings
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <ListItemIcon>
+            <Logout fontSize="small" />
+          </ListItemIcon>
+          Logout
+        </MenuItem>
+      </Menu>
+    </>
+  );
 }
