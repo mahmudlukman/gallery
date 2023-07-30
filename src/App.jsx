a698ca86bc407d4658909b74beb1e099b1de7f32
Mahmud Lukman <mistamann49@gmail.com>
2023-07-30 16:56:36 +0100

    ImagesList component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Container } from "@mui/material"
 import { Nav } from "./components/Nav"
-import ImagesList from "./components/imagesList/ImagesList"
+import {ImagesList} from "./components/imagesList/ImagesList"
 import Upload from "./components/upload/Upload"
 
 function App() {
