675743c0df6e47ea2141681884b0e4bcff40270a
Mahmud Lukman <mistamann49@gmail.com>
2023-07-31 11:56:22 +0100

    authContext created

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import { Container } from "@mui/material"
 import { Nav } from "./components/Nav"
 import {ImagesList} from "./components/imagesList/ImagesList"
 import Upload from "./components/upload/Upload"
+import AuthContext from "./context/AuthContext"
 
 function App() {
 
   return (
     <Container maxWidth='lg' sx={{textAlign: 'center', mt: '3rem'}}>
-      <Nav/>
-      <Upload/>
-      <ImagesList/>
+      <AuthContext>
+        <Nav/>
+        <Upload/>
+        <ImagesList/>
+      </AuthContext>
     </Container>
   )
 }
---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,46 @@
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut } from 'firebase/auth'
+import {createContext, useContext, useEffect, useState} from 'react'
+import { auth } from '../firebase/config'
+
+const authContext = createContext()
+
+export const useAuth = () => {
+    return useContext(authContext)
+}
+
+const AuthContext = ({children}) => {
+  const [currentUser, setCurrentUser] = useState(null)
+  const [modal, setModal] = useState({isOpen: false, title: '', content: ''})
+
+  const signUp = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password)
+  }
+
+  const login = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password)
+  }
+
+  const logout = () => {
+    return signOut(auth)
+  }
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user)
+      console.log('user status changed: ', user)
+    })
+    return unsubscribe
+  }, [])
+
+  const value = {
+    currentUser,
+    signUp,
+    login,
+    logout,
+    modal,
+    setModal,
+  }
+  return <authContext.Provider {...{value}}>{children}</authContext.Provider>
+}
+
+export default AuthContext
